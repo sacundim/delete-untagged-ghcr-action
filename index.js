4b13ff002b97a09e96ac9d4bf2e2cff4283445f4
sacundim <anybody@sacundim.org>
2023-02-20 21:46:51 -0800

    The example code pages I was mashing together did not work together at all...

--- index.js
+++ index.js
@@ -3,23 +3,34 @@ const github = require('@actions/github');
 
 try {
     const owner = core.getInput('owner');
-    const package = core.getInput('package');
+    const package_name = core.getInput('package');
     const per_page = core.getInput('per-page');
+    const token = core.getInput('token');
 
-    const response = github.request(
-        "GET /${owner}/packages/container/${package}/versions",
-        { per_page: per_page }
+    const octokit = github.getOctokit(myToken)
+    const versions = await octokit.request(
+        'GET /user/packages/{package_type}/{package_name}/versions', 
+        {
+            package_type: 'container',
+            package_name: package_name,
+            per_page: per_page,
+            state: 'active'
+        }
     );
 
-    for(version of response.data) {
+    for(version of versions) {
         if (version.metadata.container.tags.length == 0) {
-            console.log("delete " + version.id)
+            core.info("delete " + version.id)
             /*
-            const deleteResponse = github.request(
-                "DELETE /${owner}/packages/container/${package}/versions/" + version.id,
-                { }
+            const delete = await octokit.request(
+                'DELETE /user/packages/{package_type}/{package_name}/versions/{package_version_id}', 
+                {
+                    package_type: 'container',
+                    package_name: package_name,
+                    package_version_id: version_id
+                }
             );
-            console.log("status " + deleteResponse.status)
+            core.info("status " + delete.status)
             */
         }
     }
