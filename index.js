78f9506437e76a62f83e1c481e0b3fa4489a1a0e
sacundim <anybody@sacundim.org>
2023-02-20 21:51:21 -0800

    Fix: SyntaxError: await is only valid in async functions and the top level bodies of modules

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
-    const owner = core.getInput('owner');
-    const package_name = core.getInput('package');
-    const per_page = core.getInput('per-page');
-    const token = core.getInput('token');
+const owner = core.getInput('owner');
+const package_name = core.getInput('package');
+const per_page = core.getInput('per-page');
+const token = core.getInput('token');
 
-    const octokit = github.getOctokit(myToken)
-    const versions = await octokit.request(
-        'GET /user/packages/{package_type}/{package_name}/versions', 
-        {
-            package_type: 'container',
-            package_name: package_name,
-            per_page: per_page,
-            state: 'active'
-        }
-    );
-
-    for(version of versions) {
-        if (version.metadata.container.tags.length == 0) {
-            core.info("delete " + version.id)
-            /*
-            const delete = await octokit.request(
-                'DELETE /user/packages/{package_type}/{package_name}/versions/{package_version_id}', 
-                {
-                    package_type: 'container',
-                    package_name: package_name,
-                    package_version_id: version_id
-                }
-            );
-            core.info("status " + delete.status)
-            */
-        }
+const octokit = github.getOctokit(myToken)
+const versions = await octokit.request(
+    'GET /user/packages/{package_type}/{package_name}/versions', 
+    {
+        package_type: 'container',
+        package_name: package_name,
+        per_page: per_page,
+        state: 'active'
     }
-} catch (error) {
-    core.setFailed(error.message);
+);
+
+for(version of versions) {
+    if (version.metadata.container.tags.length == 0) {
+        core.info("delete " + version.id)
+        /*
+        const delete = await octokit.request(
+            'DELETE /user/packages/{package_type}/{package_name}/versions/{package_version_id}', 
+            {
+                package_type: 'container',
+                package_name: package_name,
+                package_version_id: version_id
+            }
+        );
+        core.info("status " + delete.status)
+        */
+    }
 }
